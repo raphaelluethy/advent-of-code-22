7a299fa9e9eb135a1fe93fd4ecc0fbd1e1913b7f
Raphael Lüthy <raphael@famluethy.ch>
2022-12-05 13:44:35 +0100

    added stack util

---
+++ utils/stack.ts
@@ -0,0 +1,19 @@
+export default class Stack<T> {
+  private items: T[] = [];
+
+  push(item: T) {
+    this.items.push(item);
+  }
+
+  pop(): T {
+    return this.items.pop() as T;
+  }
+
+  isEmpty(): boolean {
+    return this.size() === 0;
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+}

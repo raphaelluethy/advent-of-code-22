99565535da3cb910140b99bbe09adcc8e7ca1adf
Raphael Lüthy <raphael@famluethy.ch>
2022-12-05 12:08:20 +0100

    made a oneliner, didnt help with visibilty but hey ¯\_(ツ)_/¯

--- 1/a.ts
+++ 1/a.ts
@@ -1,18 +1,24 @@
-export const x = '';
+export const x = "";
 
-const input = await Deno.readTextFile('./input.txt');
+const input = await Deno.readTextFile("./input.txt");
 
-const split = input.split('\n');
+const split = input.split("\n");
 
 let max = -1;
 let current = 0;
 for (const line of split) {
-	if (line.length == 0) {
-		max = Math.max(max, current);
-		current = 0;
-		continue;
-	}
-	current += parseInt(line);
+  if (line.length == 0) {
+    max = Math.max(max, current);
+    current = 0;
+    continue;
+  }
+  current += parseInt(line);
 }
 
-console.log(max);
+const newMax = Math.max(
+  ...input.split("\n\n").map((blocks) =>
+    blocks.split("\n").map((x) => parseInt(x)).reduce((a, b) => a + b, 0)
+  ),
+);
+
+console.log({ max, newMax });
